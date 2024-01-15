5beefd639c774b76def4467bf7c105461aa1ddbf
Teja Puttapaka <sriteja.puttapaka@gmail.com>
2024-01-14 23:21:10 -0600

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to Java Home Page";
 	}
 
 	public static void main(String[] args) {
